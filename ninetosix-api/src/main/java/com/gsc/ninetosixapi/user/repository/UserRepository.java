2285b2b68851b57b017699aaa6e046c32ccfbb64
pdsom0321 <pds.gsitm@gmail.com>
2022-08-02 15:35:45 +0900

    JPA -> Spring Data JPA

--- ninetosix-api/src/main/java/com/gsc/ninetosixapi/user/repository/UserRepository.java
+++ ninetosix-api/src/main/java/com/gsc/ninetosixapi/user/repository/UserRepository.java
@@ -1,39 +1,10 @@
 package com.gsc.ninetosixapi.user.repository;
 
 import com.gsc.ninetosixapi.user.entity.User;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
 import java.util.Optional;
 
-@Repository
-@Transactional(readOnly = true)
-public class UserRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    @Transactional
-    public void save(User user) {
-        em.persist(user);
-    }
-
-    public User findOne(Long id) {
-        return em.find(User.class, id);
-    }
-
-    public List<User> findAll() {
-        return em.createQuery("select u from User u", User.class)
-                .getResultList();
-    }
-
-    public Optional<User> findByEmail(String email) {
-        List<User> user = em.createQuery("select u from User u where u.email = :email", User.class)
-                .setParameter("email", email)
-                .getResultList();
-        return user.stream().findAny();
-    }
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
