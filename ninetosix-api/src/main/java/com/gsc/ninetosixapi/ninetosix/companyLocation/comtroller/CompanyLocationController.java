b1121425dd3480fa0e098541342ce83935b6acff
pdsom0321 <pds.gsitm@gmail.com>
2022-10-07 16:17:38 +0900

    logback 설정 추가

--- ninetosix-api/src/main/java/com/gsc/ninetosixapi/ninetosix/companyLocation/comtroller/CompanyLocationController.java
+++ ninetosix-api/src/main/java/com/gsc/ninetosixapi/ninetosix/companyLocation/comtroller/CompanyLocationController.java
@@ -3,6 +3,7 @@ package com.gsc.ninetosixapi.ninetosix.companyLocation.comtroller;
 import com.gsc.ninetosixapi.ninetosix.companyLocation.dto.CompanyLocationsResDTO;
 import com.gsc.ninetosixapi.ninetosix.companyLocation.service.CompanyLocationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import springfox.documentation.annotations.ApiIgnore;
 import java.security.Principal;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class CompanyLocationController {
