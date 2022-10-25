123d0d08af7cee8bded0f45ab1a01e5df1b1f458
pdsom0321 <pds.gsitm@gmail.com>
2022-10-25 10:34:17 +0900

    비밀번호가 일치하지 않을 때, 존재하지 않는 아이디일 때 400 에러 처리 (기존 401의 경우, jwt토큰만료와 같은 에러코드여서)

--- ninetosix-api/src/main/java/com/gsc/ninetosixapi/core/jwt/JwtAuthenticationEntryPoint.java
+++ ninetosix-api/src/main/java/com/gsc/ninetosixapi/core/jwt/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,7 @@
 package com.gsc.ninetosixapi.core.jwt;
 
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -13,7 +15,17 @@ import java.io.IOException;
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+
+        if(authException instanceof BadCredentialsException) {
+            // 유효한 자격증명을 제공하지 않고 비밀번호가 일치하지 않을 때 400
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "비밀번호가 일치하지 않음");
+        } else if(authException instanceof InternalAuthenticationServiceException) {
+            // 유효한 자격증명을 제공하지 않고 존재하지 않는 아이디일 때 400
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "존재하지 않는 아이디");
+        } else {
+            // 유효한 자격증명을 제공하지 않고 접근하려 할 때 401
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized!");
+        }
+
     }
 }
