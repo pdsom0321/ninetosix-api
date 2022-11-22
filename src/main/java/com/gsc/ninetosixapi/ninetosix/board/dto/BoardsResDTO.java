7fdf5116ca2e0aeaaf19d29fc1ac16df745edcdf
pdsom0321 <pds.gsitm@gmail.com>
2022-11-22 17:57:18 +0900

    공지사항 목록 response에 id 추가

--- src/main/java/com/gsc/ninetosixapi/ninetosix/board/dto/BoardsResDTO.java
+++ src/main/java/com/gsc/ninetosixapi/ninetosix/board/dto/BoardsResDTO.java
@@ -7,12 +7,14 @@ import java.time.LocalDateTime;
 
 @Getter @Builder
 public class BoardsResDTO {
+    private Long id;
     private String title;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
 
     public static BoardsResDTO getBoards(Board board) {
         return BoardsResDTO.builder()
+                .id(board.getId())
                 .title(board.getTitle())
                 .startDate(board.getStartDate())
                 .endDate(board.getEndDate())
