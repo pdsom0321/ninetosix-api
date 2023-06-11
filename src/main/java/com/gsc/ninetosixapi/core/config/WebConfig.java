2a97632d1d5848919da46cdecc7427dbb29650a8
som <pds.gsitm@gmail.com>
2023-06-11 22:50:31 +0900

    profile 환경 별 설정 분리

--- src/main/java/com/gsc/ninetosixapi/core/config/WebConfig.java
+++ src/main/java/com/gsc/ninetosixapi/core/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.gsc.ninetosixapi.core.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${spring.mvc.cors.mapping}")
+    private String corsMapping;
+
+    @Value("${spring.mvc.cors.allowed-origins}")
+    private String corsAllowedOrigins;
+
+    @Value("${spring.mvc.cors.allow-credentials}")
+    private boolean corsAllowCredentials;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://deogus-dev.github.io")
-                .allowCredentials(true);
+        registry.addMapping(corsMapping)
+                .allowedOrigins(corsAllowedOrigins)
+                .allowCredentials(corsAllowCredentials);
     }
 }
