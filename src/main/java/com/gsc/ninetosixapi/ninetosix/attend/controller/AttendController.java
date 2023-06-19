780a19552c495de64605d5e29ed463efad4c21f4
pdsom0321 <pds.gsitm@gmail.com>
2023-06-19 18:11:38 +0900

    [Add] 출근부 엑셀 export

--- src/main/java/com/gsc/ninetosixapi/ninetosix/attend/controller/AttendController.java
+++ src/main/java/com/gsc/ninetosixapi/ninetosix/attend/controller/AttendController.java
@@ -9,9 +9,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Slf4j
 @RestController
@@ -39,16 +44,16 @@ public class AttendController {
 
     @ApiOperation(value = "퇴근", notes = "실행: update outTime")
     @PostMapping("attend/off")
-    public ResponseEntity offWork() {
+    public ResponseEntity<Void> offWork() {
         attendService.offWork();
-        return new ResponseEntity(HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 
     @ApiOperation(value = "휴가 및 그외 신청", notes = "attendCode, fromDate, toDate -> insert Attend 실행")
     @PostMapping("attend/{attendCode}")
-    public ResponseEntity dayOff(@PathVariable String attendCode, @RequestBody AttendCodeReqDTO reqDTO) {
+    public ResponseEntity<Void> dayOff(@PathVariable String attendCode, @RequestBody AttendCodeReqDTO reqDTO) {
         attendService.dayOff(attendCode, reqDTO);
-        return ResponseEntity.ok(HttpStatus.OK);
+        return ResponseEntity.ok().build();
     }
 
     @UserId
@@ -67,5 +72,19 @@ public class AttendController {
     public ResponseEntity<List<MonthlyResDTO>> monthlyAttendanceList(HttpServletRequest request, @PathVariable String month){
         Long memberId = (Long) request.getAttribute("memberId");
         return ResponseEntity.ok(attendService.monthlyAttendanceList(memberId, month));
+    }
+
+    @GetMapping("attend/export/{year}/{month}")
+    public ModelAndView exportAttend(@PathVariable int year, @PathVariable int month) {
+        YearMonth yearMonth = YearMonth.of(year, month);
+        int lastDayOfMonth = yearMonth.lengthOfMonth();
+
+        List<LocalDate> dates = IntStream.rangeClosed(1, lastDayOfMonth)
+                .mapToObj(yearMonth::atDay)
+                .collect(Collectors.toList());
+
+        ModelAndView mv = new ModelAndView("attendance");
+        mv.addObject("dates", dates);
+        return mv;
     }
 }
