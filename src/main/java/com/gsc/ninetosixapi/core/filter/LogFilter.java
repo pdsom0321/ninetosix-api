7e5b70826015a1152d39e8167bd2c9dd58d59019
som <pds.gsitm@gmail.com>
2023-07-20 14:39:56 +0900

    [Add] LogFilter - request, response 로깅

---
+++ src/main/java/com/gsc/ninetosixapi/core/filter/LogFilter.java
@@ -0,0 +1,92 @@
+package com.gsc.ninetosixapi.core.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Component
+public class LogFilter implements Filter {
+    private static final Logger log = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        log.info(" ================LogFilter doFilter");
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper((HttpServletResponse) response);
+
+        long start = System.currentTimeMillis();
+        chain.doFilter(requestWrapper, responseWrapper);
+        long end = System.currentTimeMillis();
+
+        log.info("┌ [●●●●●● 요청 청보 ●●●●●●]");
+        log.info("│ Request URL: {}", ((HttpServletRequest) request).getRequestURI());
+        log.info("│ Method: {}", ((HttpServletRequest) request).getMethod());
+        log.info("│ Body: {}", getRequestBody(requestWrapper));
+
+        log.info("┌ [●●●●●● 응답 청보 ●●●●●●]");
+        log.info("│ Status: {}", responseWrapper.getStatus());
+        log.info("│ Body: {}", getResponseBody(responseWrapper));
+
+        log.info("┌ [●●●●●● 요청-응답 시간 ●●●●●●]");
+        log.info("│ {}", (end - start) / 1000.0);
+
+    }
+
+    private Map getHeaders(HttpServletRequest request) {
+        Map headerMap = new HashMap<>();
+
+        Enumeration headerArray = request.getHeaderNames();
+        while (headerArray.hasMoreElements()) {
+            String headerName = (String) headerArray.nextElement();
+            headerMap.put(headerName, request.getHeader(headerName));
+        }
+        return headerMap;
+    }
+
+    private String getRequestBody(ContentCachingRequestWrapper request) {
+        ContentCachingRequestWrapper wrapper = WebUtils.getNativeRequest(request, ContentCachingRequestWrapper.class);
+        if (wrapper != null) {
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                try {
+                    return new String(buf, 0, buf.length, wrapper.getCharacterEncoding());
+                } catch (UnsupportedEncodingException e) {
+                    return " - ";
+                }
+            }
+        }
+        return " - ";
+    }
+
+    private String getResponseBody(final HttpServletResponse response) throws IOException {
+        String payload = null;
+        ContentCachingResponseWrapper wrapper =
+                WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
+        if (wrapper != null) {
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                payload = new String(buf, 0, buf.length, wrapper.getCharacterEncoding());
+                wrapper.copyBodyToResponse();
+            }
+        }
+        return null == payload ? " - " : payload;
+    }
+}
