a9f62546a9e916ad335f44f8ce323015634c07dc
som <pds.gsitm@gmail.com>
2023-07-20 15:47:22 +0900

    [Test] Swagger와 관련된 URL 제외 #3

--- src/main/java/com/gsc/ninetosixapi/core/filter/LogFilter.java
+++ src/main/java/com/gsc/ninetosixapi/core/filter/LogFilter.java
@@ -23,7 +23,7 @@ import java.util.stream.Stream;
 @Component
 public class LogFilter implements Filter {
     private static final Logger log = LoggerFactory.getLogger(LogFilter.class);
-    private final List<String> excludedUrls = Stream.of("/swagger-ui.html", "/webjars/**").toList();
+    private final List<String> excludedUrls = Stream.of("/swagger**", "/webjars/**").toList();
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
