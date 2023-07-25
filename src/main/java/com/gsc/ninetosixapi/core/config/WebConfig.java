66e51a7f4bd19bbd2c6ba636b7ceb74dabde790d
Deahyeon Jang <zdh8456@gmail.com>
2023-07-25 09:35:17 +0900

    [Modify] cross-origin 관련 httpMethod 허용 추가

--- src/main/java/com/gsc/ninetosixapi/core/config/WebConfig.java
+++ src/main/java/com/gsc/ninetosixapi/core/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.gsc.ninetosixapi.core.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,6 +19,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping(corsMapping)
                 .allowedOrigins("https://www.ninetosixapi.tk","https://deogus-dev.github.io","http://localhost:7889")
-                .allowCredentials(corsAllowCredentials);
+                .allowCredentials(corsAllowCredentials)
+                .allowedMethods(
+                    HttpMethod.GET.name(),
+                    HttpMethod.HEAD.name(),
+                    HttpMethod.POST.name(),
+                    HttpMethod.PUT.name());
+
     }
 }
