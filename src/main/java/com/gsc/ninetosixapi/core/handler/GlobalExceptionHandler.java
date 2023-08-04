9ffa69aa8992231a80e0768207c05bbf9ecce7ae
som <pds.gsitm@gmail.com>
2023-08-04 15:51:12 +0900

    [Modify] response에 모든 에러 500 처리로 변경

--- src/main/java/com/gsc/ninetosixapi/core/handler/GlobalExceptionHandler.java
+++ src/main/java/com/gsc/ninetosixapi/core/handler/GlobalExceptionHandler.java
@@ -3,8 +3,6 @@ package com.gsc.ninetosixapi.core.handler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,14 +14,13 @@ public class GlobalExceptionHandler {
         log.error("Exception occurred: ", ex);
 
         // 예외 종류에 따라 다른 응답 코드 반환
-        if (ex instanceof BadCredentialsException || ex instanceof InternalAuthenticationServiceException) {
+        /*if (ex instanceof BadCredentialsException || ex instanceof InternalAuthenticationServiceException) {
             // BadCredentialsException이 발생한 경우 401 Unauthorized 반환
             ErrorResponse errorResponse = new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), "Unauthorized", ex.getMessage());
             return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
-        } else {
-            // 그 외 다른 예외가 발생한 경우 500 Internal Server Error 반환
-            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal Server Error", ex.getMessage());
-            return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        }*/
+
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal Server Error", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
