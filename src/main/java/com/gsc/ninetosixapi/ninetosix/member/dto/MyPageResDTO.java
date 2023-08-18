0a785ca4b18fe0cad15c22abf094f9a31892deef
som <pds.gsitm@gmail.com>
2023-08-18 11:05:25 +0900

    [Add] mypage responseDTO + teamName

--- src/main/java/com/gsc/ninetosixapi/ninetosix/member/dto/MyPageResDTO.java
+++ src/main/java/com/gsc/ninetosixapi/ninetosix/member/dto/MyPageResDTO.java
@@ -2,8 +2,8 @@ package com.gsc.ninetosixapi.ninetosix.member.dto;
 
 import com.gsc.ninetosixapi.ninetosix.member.entity.Member;
 
-public record MyPageResDTO(String email, String name, String contact, String companyName) {
+public record MyPageResDTO(String email, String name, String contact, String companyName, String teamName) {
     public static MyPageResDTO of(Member member) {
-        return new MyPageResDTO(member.getEmail(), member.getName(), member.getContact(), member.getCompany().getName());
+        return new MyPageResDTO(member.getEmail(), member.getName(), member.getContact(), member.getCompany().getName(), member.getTeam().getName());
     }
 }
