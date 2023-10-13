bbae1416a1e5b8e3b24180000c30cd7f77282cee
som <pds.gsitm@gmail.com>
2023-10-13 13:23:09 +0900

    [Fix] doc path

--- src/main/java/com/gsc/ninetosixapi/ninetosix/attend/service/AttendService.java
+++ src/main/java/com/gsc/ninetosixapi/ninetosix/attend/service/AttendService.java
@@ -144,7 +144,7 @@ public class AttendService {
         List<Integer> dates = getDayOfMonth(year, month);
 
         String templateFileName = "form1.xlsx";
-        try (FileInputStream file = new FileInputStream(docPath + "/" + templateFileName);
+        try (FileInputStream file = new FileInputStream(docPath + templateFileName);
              XSSFWorkbook wb = new XSSFWorkbook(file)) {
 
             //템플릿 시트에 월 미리 세팅
