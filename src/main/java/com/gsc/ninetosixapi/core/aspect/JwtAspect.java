3808a0393dbb54061040b2409d60e62b65004899
som <pds.gsitm@gmail.com>
2023-10-25 17:37:27 +0900

    [Feat] @UserId aspect 에 비밀번호 변경 기한 확인 로직 추가

--- src/main/java/com/gsc/ninetosixapi/core/aspect/JwtAspect.java
+++ src/main/java/com/gsc/ninetosixapi/core/aspect/JwtAspect.java
@@ -1,5 +1,7 @@
 package com.gsc.ninetosixapi.core.aspect;
 
+import com.gsc.ninetosixapi.ninetosix.member.entity.Member;
+import com.gsc.ninetosixapi.ninetosix.member.service.MemberService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
@@ -14,16 +16,23 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.security.Key;
+import java.time.LocalDateTime;
 
 @Aspect
 @Component
 public class JwtAspect {
     public static final String AUTHORIZATION_HEADER = "Authorization";
+
     public static final String BEARER_PREFIX = "Bearer ";
+
     private final Key key;
 
-    public JwtAspect(@Value("${jwt.secret}") String secretKey) {
+    private final MemberService memberService;
+
+    public JwtAspect(@Value("${jwt.secret}") String secretKey, MemberService memberService) {
+        this.memberService = memberService;
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         this.key = Keys.hmacShaKeyFor(keyBytes);
     }
@@ -39,6 +48,17 @@ public class JwtAspect {
             Long memberId = claims.get("id", Long.class);
 
             request.setAttribute("memberId", memberId);
+
+            // 비밀번호 변경 기한 확인 로직 추가 2023-10-25
+            Member member = memberService.findById(memberId);
+            LocalDateTime passwordExpiryDate = member.getPasswordExpiryDate();
+            if(passwordExpiryDate.isBefore(LocalDateTime.now())) {
+                HttpServletResponse response = ((ServletRequestAttributes) requestAttributes).getResponse();
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.getWriter().write("Password has expired.");
+
+                return null; // 메서드 실행 중단
+            }
         }
 
         return joinPoint.proceed();
